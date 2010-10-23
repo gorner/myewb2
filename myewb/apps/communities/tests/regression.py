df550c04a8c6aa6e900632860f4af319638d87d4
Joshua Gorner <joshuagorner@ewb.ca>
2010-10-23 19:32:30 -0400

    Bug #679 (Can't remove members from group): adds a link to remove each member on the /members/ full list for each group. The system is already set up to allow admins to remove members, but using if-else around applicable wording when that is the case so admins don't think they're removing themselves. Also have added some regression testing for this, which I suspect may be a bit overkill, but it's there now. ;-)

--- myewb/apps/communities/tests/regression.py
+++ myewb/apps/communities/tests/regression.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
+from django.test.client import Client
 
 from django.contrib.auth.models import User
 from communities.models import Community
+
+from base_groups.models import GroupMember, GroupMemberRecord
 
 class TestAddCreatorToCommunity(TestCase):
     """
@@ -14,4 +17,75 @@ class TestAddCreatorToCommunity(TestCase):
         com = Community.objects.create(slug='new-com', name='new-com', creator=u)
         self.assertTrue(com.user_is_member(u))
         self.assertTrue(com.user_is_admin(u))
+        
+class TestCommunityMembershipDeletion(TestCase):
+    """
+    Testing the ability for users to be removed from groups, by themselves or others.
+    """
+
+    def setUp(self):
+        self.admin = User.objects.create_user(username='creator', email='c@ewb.ca', password='password')
+        self.user = User.objects.create_user(username='user', email='user@ewb.ca', password='password')
+
+        self.bg = Community.objects.create(slug='bg', name='generic community', creator=self.admin)
+        self.gm = GroupMember.objects.create(user=self.user, group=self.bg)
+
+    def tearDown(self):
+        GroupMemberRecord.objects.all().delete()
+        GroupMember.objects.all().delete()
+        Community.objects.all().delete()
+        User.objects.all().delete()
+
+    def test_admin_member_removal(self):
+        """
+        Group admin can remove a member.
+        """
+        c = Client()
+        response = c.post('/account/login/', {'login_name': 'creator', 'password': 'password'})
+        response = c.get('/communities/bg/members/user/delete/')
+        self.assertContains(response, "Are you sure you want to remove user@... from generic community?")
+
+        response = c.post('/communities/bg/members/user/delete/')
+        self.assertEqual(0, GroupMember.objects.filter(user=self.user, group=self.bg).count())
+
+        c.logout()
+
+    def test_self_member_removal(self):
+        """
+        Member can end own membership.
+        """
+        c = Client()
+        response = c.post('/account/login/', {'login_name': 'user', 'password': 'password'})
+        response = c.get('/communities/bg/members/user/delete/')
+        self.assertContains(response, "Are you sure you want to leave generic community?")
+
+        response = c.post('/communities/bg/members/user/delete/')
+        self.assertEqual(0, GroupMember.objects.filter(user=self.user, group=self.bg).count())
+
+        c.logout()
+
+    def test_third_party_member_removal(self):
+        """
+        A user that is not a group admin cannot end another user's membership.
+        """
+        c = Client()
+        response = c.post('/account/login/', {'login_name': 'user', 'password': 'password'})
+        response = c.get('/communities/bg/members/creator/delete/')
+        self.assertNotContains(response, "Are you sure you want to remove c@... from generic community?")
+
+        response = c.post('/communities/bg/members/creator/delete/')
+        self.assertEqual(1, GroupMember.objects.filter(user=self.admin, group=self.bg).count())
+        
+        c.logout()
+
+    def test_anon_member_removal(self):
+        """
+        An anonymous user cannot end another user's membership.
+        """
+        c = Client()
+        response = c.get('/communities/bg/members/creator/delete/')
+        self.assertNotContains(response, "Are you sure you want to remove c@... from generic community?", status_code=302)
+
+        response = c.post('communities/bg/members/creator/delete/')
+        self.assertEqual(1, GroupMember.objects.filter(user=self.admin, group=self.bg).count())
 
--- myewb/apps/networks/tests/regression.py
+++ myewb/apps/networks/tests/regression.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
+from django.test.client import Client
 
 from django.contrib.auth.models import User
 from networks.models import Network
+
+from base_groups.models import GroupMember, GroupMemberRecord
 
 class TestAddCreatorToNetwork(TestCase):
     """
@@ -15,3 +18,73 @@ class TestAddCreatorToNetwork(TestCase):
         self.assertTrue(net.user_is_member(u))
         self.assertTrue(net.user_is_admin(u))
 
+class TestNetworkMembershipDeletion(TestCase):
+    """
+    Testing the ability for users to be removed from groups, by themselves or others.
+    """
+
+    def setUp(self):
+        self.admin = User.objects.create_user(username='creator', email='c@ewb.ca', password='password')
+        self.user = User.objects.create_user(username='user', email='user@ewb.ca', password='password')
+
+        self.bg = Network.objects.create(slug='bg', name='generic network', creator=self.admin)
+        self.gm = GroupMember.objects.create(user=self.user, group=self.bg)
+
+    def tearDown(self):
+        GroupMemberRecord.objects.all().delete()
+        GroupMember.objects.all().delete()
+        Network.objects.all().delete()
+        User.objects.all().delete()
+
+    def test_admin_member_removal(self):
+        """
+        Group admin can remove a member.
+        """
+        c = Client()
+        response = c.post('/account/login/', {'login_name': 'creator', 'password': 'password'})
+        response = c.get('/networks/bg/members/user/delete/')
+        self.assertContains(response, "Are you sure you want to remove user@... from generic network?")
+
+        response = c.post('/networks/bg/members/user/delete/')
+        self.assertEqual(0, GroupMember.objects.filter(user=self.user, group=self.bg).count())
+
+        c.logout()
+
+    def test_self_member_removal(self):
+        """
+        Member can end own membership.
+        """
+        c = Client()
+        response = c.post('/account/login/', {'login_name': 'user', 'password': 'password'})
+        response = c.get('/networks/bg/members/user/delete/')
+        self.assertContains(response, "Are you sure you want to leave generic network?")
+
+        response = c.post('/networks/bg/members/user/delete/')
+        self.assertEqual(0, GroupMember.objects.filter(user=self.user, group=self.bg).count())
+
+        c.logout()
+
+    def test_third_party_member_removal(self):
+        """
+        A user that is not a group admin cannot end another user's membership.
+        """
+        c = Client()
+        response = c.post('/account/login/', {'login_name': 'user', 'password': 'password'})
+        response = c.get('/networks/bg/members/creator/delete/')
+        self.assertNotContains(response, "Are you sure you want to remove c@... from generic network?")
+
+        response = c.post('/networks/bg/members/creator/delete/')
+        self.assertEqual(1, GroupMember.objects.filter(user=self.admin, group=self.bg).count())
+        
+        c.logout()
+        
+    def test_anon_member_removal(self):
+        """
+        An anonymous user cannot end another user's membership.
+        """
+        c = Client()
+        response = c.get('/networks/bg/members/creator/delete/')
+        self.assertNotContains(response, "Are you sure you want to remove c@... from generic network?", status_code=302)
+
+        response = c.post('/networks/bg/members/creator/delete/')
+        self.assertEqual(1, GroupMember.objects.filter(user=self.admin, group=self.bg).count())
